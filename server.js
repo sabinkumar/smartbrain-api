2631839fb1c932982f44ded89c96332969f548ac
sabinyadav <sabin_sky1@hotmail.com>
2019-04-01 06:01:53 +0545

    Upadting

--- server.js
+++ server.js
@@ -11,10 +11,9 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : process.env.DATABASE_URL,
     user : 'postgres',
-    password : 'sabin123',
-    database : 'smartbrain'
+    ssl: true
   }
 });
 
